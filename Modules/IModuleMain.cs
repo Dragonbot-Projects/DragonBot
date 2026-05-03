f53d46b8e7e9fccdf5aa6d1fd0b0f6abd303e2c2
Adraxas Nazaron <dragonjunk45@gmail.com>
2026-05-03 12:32:00 -0700

    Added interface for ModuleMain

---
+++ Modules/IModuleMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragonBot.Modules
+{
+    public interface IModuleMain
+    {
+        public abstract static Dictionary<Type, Action<object>> Initilizers { get; }
+        public abstract static void InitAssembly();
+    }
+}
